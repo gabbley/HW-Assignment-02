23d62f4dae6555c96cc43d29ecd5f956b0c604c8
gabbley <baniquedg@bishopireton.org>
2017-10-04 11:06:33 -0400

    Utilizes output.txt

--- Sorts/src/CardTester.java
+++ Sorts/src/CardTester.java
@@ -1,21 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
 
 public class CardTester {
 
 	public static void main(String[] args) {
+		PrintWriter out = null;
+		
+		try {
+			out = new PrintWriter("output.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 
 		// Card testCard = new Card();
 		// System.out.println(testCard);
 
 		Deck testDeck = new Deck();
-//		testDeck.shuffle();
+		// testDeck.shuffle();
 		// System.out.println(testDeck);
 
-//		testDeck.selectionSort();
-		System.out.println(testDeck);
+		// testDeck.selectionSort();
+		out.println(testDeck);
 
-		System.out.println(Deck.pick() + "\n");
-		System.out.println(testDeck);
-		System.out.println(testDeck.deal(2, 3)[0].getDeck());
+		out.println(Deck.pick() + "\n");
+		out.println(testDeck);
+		//out.println(testDeck.deal(2, 3)[0].getDeck());
+		
+		out.close();
 
 	}
 
